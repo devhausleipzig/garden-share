b17987ee0506de6a34b8c50eaae53a23e6320395
Simon Börner <boernersimon@googlemail.com>
2022-04-12 15:27:04 +0200

    Finish Header Component

--- components/header/Header.stories.tsx
+++ components/header/Header.stories.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { Header } from "../components/header";
-
+import { Header } from "./header";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
