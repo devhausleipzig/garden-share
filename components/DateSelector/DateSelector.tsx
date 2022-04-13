cdbef8e1f0e1a1f4786a440dcc08dd617a961a6a
Simon Börner <boernersimon@googlemail.com>
2022-04-13 10:26:38 +0200

    Complete date selector

--- components/DateSelector/DateSelector.tsx
+++ components/DateSelector/DateSelector.tsx
@@ -1,20 +1,37 @@
 import { Box, HStack, Select, Image, IconButton } from "@chakra-ui/react";
-import { TriangleDownIcon } from "@chakra-ui/icons";
+import { AddIcon, TriangleDownIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 const DateSelector = () => {
+  const [yearNumber, setYearNumber] = useState<number>(2022);
+
   return (
-    <HStack>
-      <HStack>
+    <HStack justify={"space-between"}>
+      <HStack spacing={4}>
         <IconButton
+          color="#FFFBFA"
+          bg="#27BBAD"
           aria-label="Previous Month"
           icon={<TriangleDownIcon style={{ transform: "rotate(90deg)" }} />}
           isRound
+          onClick={() => {
+            if (yearNumber >= 2018) {
+              setYearNumber(yearNumber - 1);
+            }
+          }}
         />
-
+        <p>{yearNumber}</p>
         <IconButton
-          aria-label="Previous Month"
+          color="#FFFBFA"
+          bg="#27BBAD"
+          aria-label="Next Month"
           icon={<TriangleDownIcon style={{ transform: "rotate(-90deg)" }} />}
           isRound
+          onClick={() => {
+            if (yearNumber < 2022) {
+              setYearNumber(yearNumber + 1);
+            }
+          }}
         />
       </HStack>
       <Select variant="filled" placeholder="Select Month" w="20%">
@@ -31,6 +48,13 @@ const DateSelector = () => {
         <option value="option11">November</option>
         <option value="option12">December</option>
       </Select>
+      <IconButton
+        color="#FFFBFA"
+        bg="#27BBAD"
+        aria-label="Add Message"
+        icon={<AddIcon />}
+        isRound
+      />
     </HStack>
   );
 };
