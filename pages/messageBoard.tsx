82acb29298e87d2ba5ebf6d59a0ceafa8e57d29d
inpatr <i.traub@posteo.de>
2022-04-13 09:00:29 +0200

    commit to switch branches

--- pages/messageBoard.tsx
+++ pages/messageBoard.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from "next";
-import EmojiReaction from "../components/MockHeader/EmojiReaction";
+import EmojiReaction from "../components/EmojiReaction/EmojiReaction";
 import MockHeader from "../components/MockHeader/MockHeader";
 
 const MessageBoard: NextPage = () => {
-  return <div>
-
-<EmojiReaction />
-
-  </div>;
+  return (
+    <div>
+      <EmojiReaction />
+    </div>
+  );
 };
 
 export default MessageBoard;
