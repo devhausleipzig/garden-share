cef9221a8b16417ddfe255d00c6b6410d8fcb349
Tanous Atallah <tanous.atallah@gmail.com>
2022-04-14 16:51:30 +0200

    component created, styled, props etc

--- components/WeatherWidget/WeatherWidget.tsx
+++ components/WeatherWidget/WeatherWidget.tsx
@@ -1,9 +1,44 @@
+import { Box, Flex, Image,Spacer, Text } from "@chakra-ui/react";
 
-export default function WeatherWidget(){
+interface WeatherWidgetProps{
+    bgImg:string,
+    icon:string,
+    temperature:number,
+    wind:number,
+    minTemp:number,
+    maxTemp:number,
+    precipitation: number
+}
 
-    return(
-        <div>
-            WeatherWidget
-        </div>
+
+export default function WeatherWidget({bgImg,icon,temperature, wind, minTemp, maxTemp, precipitation}:WeatherWidgetProps) {
+
+
+    return (
+        <Box minH={150} minW={200} maxH={200} maxW={300} rounded={'md'} bgSize={'cover'} bgPos={'center'} bgImage={bgImg}>
+            <Flex flexDirection={'column'} gap={5} padding={6}   >
+                <Flex justifyContent={'space-between'} >
+                    <Image  src={icon} bgPos={'center'} bgSize={'contain'} alt={''} />
+                    <Spacer></Spacer>
+                    <Box fontWeight={'extrabold'} > { `${temperature}°`} </Box>
+                    <Spacer></Spacer>
+                </Flex>
+                <Flex justify={'space-between'}>
+                <Box fontWeight={'bold'} textAlign={'center'}>
+                        <Text> wind </Text>
+                        <Text> {wind} </Text>
+                    </Box>
+                    <Box fontWeight={'bold'} textAlign={'center'} >
+                        <Text> Min / Max </Text>
+                        <Text> { `${minTemp} / ${maxTemp}`} </Text>
+                    </Box>
+                    <Box fontWeight={'bold'} textAlign={'center'}>
+                        <Text> Precipitation </Text>
+                        <Text> { `${precipitation}%`} </Text>
+                    </Box>
+
+                </Flex>
+            </Flex>
+        </Box>
     );
 }
--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,8 +1,21 @@
 import type { NextPage } from "next";
 import MockHeader from "../components/MockHeader/MockHeader";
+import WeatherWidget from "../components/WeatherWidget/WeatherWidget";
+
+const weatherValuesTEST = {
+  bgImg: "/images/sunny.webp",
+  icon: '/addIcon.svg',
+  temperature: 100,
+  wind: 0,
+  minTemp: 50,
+  maxTemp: 100,
+  precipitation: 90
+}
 
 const Calendar: NextPage = () => {
-  return <div>test</div>;
+  return <div>test
+    <WeatherWidget {...weatherValuesTEST} />
+  </div>;
 };
 
 export default Calendar;
