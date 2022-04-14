128f16453510ca25e49e0fd065ed3076b40d3680
Dan McAtee <danielmcatee@me.com>
2022-04-14 10:48:45 +0200

    add leapyear example

---
+++ testing-examples/leapYear.ts
@@ -0,0 +1,10 @@
+export const leapYear = (year: number) => {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  }
+  return false;
+};
