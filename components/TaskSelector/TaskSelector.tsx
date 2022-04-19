3a91d863e2e162e661ade45aea542cda5ad13c34
Dan McAtee <danielmcatee@me.com>
2022-04-19 09:28:37 +0200

    simplify component

--- components/TaskSelector/TaskSelector.tsx
+++ components/TaskSelector/TaskSelector.tsx
@@ -1,21 +1,6 @@
-import type { NextPage } from "next";
-import {
-  Stack,
-  HStack,
-  VStack,
-  Box,
-  Collapse,
-  Button,
-  Image,
-  Center,
-  Flex,
-  Text,
-  Spacer,
-  IconButton,
-} from "@chakra-ui/react";
-import Link from "next/link";
+import { CheckIcon } from "@chakra-ui/icons";
+import { HStack, Text } from "@chakra-ui/react";
 import React from "react";
-import TaskSelectorIcons from "./TaskSelectorIcons";
 import TaskTypeIcons from "./TaskTypeIcons";
 
 type User = {
@@ -54,63 +39,48 @@ const TaskSelector = ({
   const [select, setSelect] = React.useState(false);
   const handleToggle = () => setSelect(!select);
 
+  const handleClick = () => {
+    if (!taken) setSelect(!select);
+  };
+
   return (
-    <>
-      <Center>
-        <Flex
-          backgroundColor={
-            taken
-              ? colors.takenbg
-              : select
-              ? colors.selectedbg
-              : colors.unselectedbg
-          }
-          w="66%"
-          px="6"
-          py="6"
-          flexDirection="column"
-          gap="2"
-          borderRadius={15}
-          boxShadow="md"
-          textColor={
-            taken
-              ? colors.takentext
-              : select
-              ? colors.selectedtext
-              : colors.unselectedtext
-          }
-        >
-          <Flex justify="space-between" align="center" minHeight="40px" gap="4">
-            {tasktype === "watering" && <TaskTypeIcons iconName={tasktype} />}
-            {tasktype === "weeding" && <TaskTypeIcons iconName={tasktype} />}
-            {tasktype === "pruning" && <TaskTypeIcons iconName={tasktype} />}
-            {tasktype === "building" && <TaskTypeIcons iconName={tasktype} />}
-            {!taken && <Text userSelect="none">{title}</Text>}
-            {taken && (
-              <Text userSelect="none" as="s">
-                {title}
-              </Text>
-            )}
-            <Spacer></Spacer>
-            {!taken && (
-              <IconButton
-                variant="unstyled"
-                aria-label={"checked-status"}
-                onClick={handleToggle}
-                _focus={{
-                  boxShadow: "none",
-                }}
-              >
-                <TaskSelectorIcons
-                  iconName={!select ? "unselected" : "selected"}
-                />
-              </IconButton>
-            )}
-            {taken && <Text userSelect="none">Already Taken</Text>}
-          </Flex>
-        </Flex>
-      </Center>
-    </>
+    <HStack
+      onClick={handleClick}
+      cursor={taken ? "not-allowed" : "pointer"}
+      backgroundColor={
+        taken
+          ? colors.takenbg
+          : select
+          ? colors.selectedbg
+          : colors.unselectedbg
+      }
+      p="6"
+      justifyContent="space-between"
+      borderRadius={15}
+      boxShadow="md"
+      textColor={
+        taken
+          ? colors.takentext
+          : select
+          ? colors.selectedtext
+          : colors.unselectedtext
+      }
+    >
+      <HStack justify="space-between" align="center" minHeight="40px" gap="4">
+        {tasktype === "watering" && <TaskTypeIcons iconName={tasktype} />}
+        {tasktype === "weeding" && <TaskTypeIcons iconName={tasktype} />}
+        {tasktype === "pruning" && <TaskTypeIcons iconName={tasktype} />}
+        {tasktype === "building" && <TaskTypeIcons iconName={tasktype} />}
+        {!taken && <Text userSelect="none">{title}</Text>}
+        {taken && (
+          <Text userSelect="none" as="s">
+            {title}
+          </Text>
+        )}
+      </HStack>
+      {!taken && (select ? <CheckIcon /> : null)}
+      {taken && <Text userSelect="none">Already Taken</Text>}
+    </HStack>
   );
 };
 
