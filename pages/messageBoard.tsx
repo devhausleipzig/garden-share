0fb4aebd60bcd0e7d4ca428a9ea06fa0f6c02e13
JonathanWagne <jonathan_wagner@hotmail.de>
2022-04-28 09:47:36 +0200

    add page

--- pages/messageBoard.tsx
+++ pages/messageBoard.tsx
@@ -1,32 +1,26 @@
 import type { NextPage } from "next";
 import { useState, useRef } from "react";
 import EmojiReaction from "../components/EmojiReaction/EmojiReaction";
-import MockHeader from "../components/MockHeader/MockHeader";
+// import MockHeader from "../components/MockHeader/MockHeader";
+import DateSelector from "../components/DateSelector/DateSelector";
+import MessageCard from "../components/MessageCard/MessageCard";
 
 const MessageBoard: NextPage = () => {
-  const [show, setShow] =useState(false)
-  const ref = useRef(null);
-
   return (
-    <div>
-      <EmojiReaction userId={"id1"} emojiProps={[
-        {
-          users:[],
-          emoji: "🐑",
-          count: 1,
-          emojiID: '',
-        },
-        {
-          users:[],
-          emoji: "😁",
-          count: 2,
-          emojiID: '',
-        }
-      ]} />
-    </div>
+    <>
+      <DateSelector />
+      <MessageCard
+        type={"LargeCard"}
+        title={"test"}
+        content={"test1"}
+        date={"03.10.2022"}
+        user={{
+          name: "Dan",
+          image: "none",
+        }}
+      />
+    </>
   );
-
 };
 
 export default MessageBoard;
-
